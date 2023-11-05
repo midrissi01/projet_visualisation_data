1d4d1cb644809ad41ea02f669934c55a707cf26a
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2023-11-05 13:41:27 +0100

    second commit

--- data_visualisation/data_app/urls.py
+++ data_visualisation/data_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='data_app_index'),
+     path('', views.index, name='data_app_index'),
+    path('page1/', views.page1, name='data_app_page1'),
    
       
 ]
--- data_visualisation/data_app/views.py
+++ data_visualisation/data_app/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render, redirect,HttpResponseRedirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+import pandas as pd
+import io
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 def index(request):
-    return render(request, 'data_app/index.html')
+    return render("data_app/index.html")
+def page1(request):
+    if request.method == 'POST':
+        # Get the uploaded file from the request
+        csv_file = request.FILES['csv_file']
+        
+        if csv_file:
+            # Read the CSV file using Pandas
+            df = pd.read_csv(csv_file)
+
+            # Process the data (e.g., select the specified line and column)
+            selected_line = request.POST['selected_line']
+            selected_column = request.POST['selected_column']
+            data = df.loc[df.index[int(selected_line)], selected_column]
+
+            # Generate a bar plot
+            plt.bar(data.index, data)
+            plt.xlabel('X-Axis')
+            plt.ylabel('Y-Axis')
+            plt.title('Bar Plot')
+
+            # Convert the plot to a BytesIO object
+            buffer = BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)
+            plt.close()
+
+            # Embed the plot in the HTML response
+            plot_data = base64.b64encode(buffer.read()).decode()
+            return render(request, 'data_app/page1.html', {'plot_data': plot_data})
+
+    # Handle GET requests
+    return render(request, 'data_app/page1.html')
